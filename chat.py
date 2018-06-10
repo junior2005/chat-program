b66cdc8ef45639acae48c6455a231eb06fb7b5a8
Nicholas Theisen <nicholas.junior.3141@gmail.com>
2018-06-09 20:04:47 -0500

    Updated

---
+++ chat.py
@@ -0,0 +1,9 @@
+import socket
+
+#If colorama is not installed the program will exit
+try:
+	import colorama
+except:
+	print("Colorama not installed")
+	print("Please install to continue")
+	exit()
